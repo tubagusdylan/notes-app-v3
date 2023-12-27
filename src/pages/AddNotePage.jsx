b1e4423c6e2af4c9df4f853fcdbaaf0b2f686c8c
tubagusdylan <tubagus.dylanr@gmail.com>
2023-12-27 15:50:29 +0700

    fix: locale context at addnotepage

--- src/pages/AddNotePage.jsx
+++ src/pages/AddNotePage.jsx
@@ -1,11 +1,14 @@
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
 import { useInput } from "../utils/customHooks";
 import { addNote } from "../utils/network-data";
+import LocaleContext from "../utils/LocaleContext";
 import "../styles/new-notes.css";
 
 const AddNotePage = () => {
   const [title, onTitleChange] = useInput("");
   const [body, onBodyChange] = useInput("");
+  const { locale } = useContext(LocaleContext);
   const navigate = useNavigate();
 
   const onAddNewNote = async (event) => {
@@ -18,10 +21,10 @@ const AddNotePage = () => {
 
   return (
     <div className="container">
-      <h1>Catat segera jangan sampai lupa!</h1>
+      <h1>{locale === "id" ? "Catat segera jangan sampai lupa!" : "Don't forget to write your note"}</h1>
       <form onSubmit={onAddNewNote}>
-        <input type="text" name="title" id="title" className="title-input" placeholder="Ini Judul Catatan" onChange={onTitleChange} value={title} />
-        <textarea name="body" id="body" className="body-input" placeholder="Ini Isi Catatan" rows={12} onChange={onBodyChange} value={body}></textarea>
+        <input type="text" name="title" id="title" className="title-input" placeholder={locale === "id" ? "Ini Judul Catatan" : "This is your note title"} onChange={onTitleChange} value={title} />
+        <textarea name="body" id="body" className="body-input" placeholder={locale === "id" ? "Ini Isi Catatan" : "This is your note body"} rows={12} onChange={onBodyChange} value={body}></textarea>
         <button className="button">
           <i className="fa-solid fa-check"></i>
         </button>
